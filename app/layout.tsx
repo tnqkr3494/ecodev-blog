a7cc0f667b0c78cf1837cf44ad378cf185c13edd
InGwonKang <tnqkr3494@naver.com>
2024-03-20 20:36:16 +0900

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#ccd5ae]`}>{children}</body>
+      <body className={`${inter.className} bg-[#ccd5ae]`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
