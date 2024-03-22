2d54e2d0da2252f29123527015ad838fb4b94f42
InGwonKang <tnqkr3494@naver.com>
2024-03-22 11:12:02 +0900

    page_posts

---
+++ app/posts/page.tsx
@@ -0,0 +1,15 @@
+import Thumbnail from "@/components/post-thumbnail";
+
+export default function Posts() {
+  return (
+    <div className="mt-32 flex px-20 mx-auto flex-wrap">
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+    </div>
+  );
+}
---
+++ components/post-thumbnail.tsx
@@ -0,0 +1,9 @@
+export default function Thumbnail() {
+  return (
+    <div className="w-1/4 pl-3 float-left py-10">
+      <img src="/img1.png"></img>
+      <h3 className="font-semibold text-2xl">title - tag[]</h3>
+      <span>2001.08.20</span>
+    </div>
+  );
+}
