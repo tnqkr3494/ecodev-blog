05c8ff3db3b3ccb5e19b23c3d9519759e2c9e538
InGwonKang <tnqkr3494@naver.com>
2024-03-25 20:49:37 +0900

    about_me_chat_fin

--- components/chatbubble.tsx
+++ components/chatbubble.tsx
@@ -1,17 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function ChatBubble() {
+  const [intersect, setIntersect] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return; // null 체크
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        setIntersect(entry.isIntersecting); // IntersectionObserver 결과를 상태에 반영
+      });
+    });
+
+    observer.observe(ref.current);
+
+    return () => {
+      observer.unobserve(ref.current!); // null 체크 후 사용
+    };
+  }, []);
   return (
-    <div className="flex flex-col gap-4">
-      <div className="chat chat-start">
+    <div ref={ref} className="flex flex-col gap-4">
+      <div
+        className={`chat chat-start ${
+          intersect ? "animate-fade-up animate-duration-300" : "opacity-0"
+        }`}
+      >
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img alt="Tailwind CSS chat bubble component" src="/kang.png" />
           </div>
         </div>
-        <div className="chat-bubble ">
+        <div className="chat-bubble">
           위 기술들은 제가 좋아하고, 즐겨 사용하는 스택들입니다.
         </div>
       </div>
-      <div className="chat chat-start">
+      <div
+        className={`chat chat-start ${
+          intersect
+            ? "animate-fade-up animate-duration-300 animate-delay-[500ms]"
+            : "opacity-0"
+        }`}
+      >
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img alt="Tailwind CSS chat bubble component" src="/kang.png" />
@@ -22,7 +52,13 @@ export default function ChatBubble() {
           공부하고있습니다.
         </div>
       </div>
-      <div className="chat chat-start">
+      <div
+        className={`chat chat-start ${
+          intersect
+            ? "animate-fade-up animate-duration-300 animate-delay-[1000ms]"
+            : "opacity-0"
+        }`}
+      >
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img alt="Tailwind CSS chat bubble component" src="/kang.png" />
