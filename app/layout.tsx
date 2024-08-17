a4e45a930167a2bf1e14cec3759da2136c31cd1d
InGwonKang <tnqkr3494@naver.com>
2024-08-17 19:48:48 +0900

    chore: Metadata url, icon 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Nanum_Gothic } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/headers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const nanum = Nanum_Gothic({
   weight: ["400", "700", "800"],
@@ -20,16 +22,16 @@ export const metadata: Metadata = {
     description:
       "프론트엔드 개발자 강인권의 개인 블로그입니다. 같이 개발 공부하고, 성장하고 싶습니다!",
     type: "website",
-    url: "",
+    url: "https://ecodev-blog.vercel.app",
     locale: "ko_KR",
     siteName: "강인권블로그",
   },
   authors: [{ name: "EcoDev" }, { name: "InGwonKang" }],
   creator: "InGwonKang",
   publisher: "InGwonKang",
-  // icons: {
-  //   icon: "/ecodev-icon.ico",
-  // },
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -42,6 +44,8 @@ export default function RootLayout({
       <body className={`${nanum.className}`}>
         <Header />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
