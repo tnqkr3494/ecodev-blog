364e3984d6629a3be7bf72144bc2a108b03719d8
InGwonKang <tnqkr3494@naver.com>
2024-08-18 11:47:40 +0900

    fix: Post페이지 revalidate 6시간으로 세팅

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -5,6 +5,8 @@ export const metadata = {
   description: "개발 공부를 하면서 기록했던 내용들입니다.",
 };
 
+export const revalidate = 21600; // 6시간마다 revalidate해서 notion API 호출(새로운 포스트 올라옴)
+
 export default function PostsLayout({
   children,
 }: {
