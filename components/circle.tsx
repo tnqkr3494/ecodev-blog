59b56cfc0fe02d869ce972f6c86377afd64afb98
InGwonKang <tnqkr3494@naver.com>
2024-08-22 21:51:01 +0900

    chore: 프로젝트 주요기능 수정

--- components/circle.tsx
+++ components/circle.tsx
@@ -11,7 +11,7 @@ export default function IconCirlce({ link, name }: ICircle) {
       className="bg-card p-2 rounded-md hover:scale-125 transition-transform tooltip"
       data-tip={name}
     >
-      <div className="w-14 h-14">
+      <div className="w-14 h-14 flex justify-center items-center">
         <Image
           src={link}
           alt="circle"
